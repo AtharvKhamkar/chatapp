3c2882d6ade510dde167965852a2391a063fe7f2
Mahesh <maheshkarande1985@gmail.com>
2022-09-21 00:01:52 +0530

    added registerpage

--- lib/pages/auth/register_page.dart
+++ lib/pages/auth/register_page.dart
@@ -1,20 +1,20 @@
-import 'package:chatapp/pages/auth/register_page.dart';
+import 'package:chatapp/pages/auth/login_page.dart';
 import 'package:chatapp/widgets/widgets.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<RegisterPage> createState() => _RegisterPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _RegisterPageState extends State<RegisterPage> {
   final formkey = GlobalKey<FormState>();
   String email = "";
   String Password = "";
-
+  String fullname = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,10 +32,30 @@ class _LoginPageState extends State<LoginPage> {
                   style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
                 ),
                 const SizedBox(height: 10),
-                const Text("Login now to see what they are talking",
+                const Text("Create your account now to chat and explore",
                     style:
                         TextStyle(fontSize: 15, fontWeight: FontWeight.w400)),
-                Image.asset("assets/login.png"),
+                Image.asset("assets/register.png"),
+                TextFormField(
+                    decoration: textInputDecoration.copyWith(
+                        labelText: "Full name",
+                        prefixIcon: Icon(
+                          Icons.person,
+                          color: Theme.of(context).primaryColor,
+                        )),
+                    onChanged: (val) {
+                      setState(() {
+                        fullname = val;
+                      });
+                    },
+                    validator: (val) {
+                      if (val!.isNotEmpty) {
+                        return null;
+                      } else {
+                        return "Name cannot be empty";
+                      }
+                    }),
+                SizedBox(height: 15),
                 TextFormField(
                     decoration: textInputDecoration.copyWith(
                         labelText: "Email",
@@ -91,11 +111,11 @@ class _LoginPageState extends State<LoginPage> {
                       ),
                     ),
                     child: const Text(
-                      "Sign In",
+                      "Register",
                       style: TextStyle(color: Colors.white, fontSize: 16),
                     ),
                     onPressed: () {
-                      login();
+                      register();
                     },
                   ),
                 ),
@@ -103,17 +123,17 @@ class _LoginPageState extends State<LoginPage> {
                   height: 10,
                 ),
                 Text.rich(TextSpan(
-                  text: "Don't have an account?",
+                  text: "Already have an account?",
                   style: TextStyle(color: Colors.black, fontSize: 14),
                   children: <TextSpan>[
                     TextSpan(
-                        text: "Register here",
+                        text: "Login now",
                         style: const TextStyle(
                             color: Colors.black,
                             decoration: TextDecoration.underline),
                         recognizer: TapGestureRecognizer()
                           ..onTap = () {
-                            nextScreen(context, RegisterPage());
+                            nextScreen(context, const LoginPage());
                           })
                   ],
                 )),
@@ -125,5 +145,5 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  login() {}
+  register() {}
 }
---
+++ lib/service/auth_service.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class AuthService {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  // Login function
+
+  //register function
+  Future registerUserWithEmailandPassword(
+      String fullname, String email, String password) async {
+    try {
+      User user = (await firebaseAuth.createUserWithEmailAndPassword(
+              email: email, password: password))
+          .user!;
+
+      if (user != null) {
+        // call our database service to update user data
+
+        return true;
+      }
+    } on FirebaseAuthException catch (e) {
+      print(e);
+    }
+  }
+
+  //signout
+}
